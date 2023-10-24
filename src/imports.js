59d65264c516063ef6cd5c1968fe23fd00bbb047
Marvin Artiga AG230133 <marvin.artigagonzalez@gmail.com>
2023-10-23 22:21:43 -0600

    nuevo formulario con bootstrap

--- src/imports.js
+++ src/imports.js
@@ -1,44 +1,76 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./registro.css";
 
 function Registro() {
   const [isLoginForm, setIsLoginForm] = useState(true);
-  const formRef = useRef(null);
 
   const toggleForm = () => {
     setIsLoginForm((prevIsLoginForm) => !prevIsLoginForm);
-
-    if (formRef.current) {
-      const form = formRef.current;
-      form.style.height = form.style.height === '0px' ? 'auto' : '0px';
-      form.style.opacity = form.style.opacity === '0' ? '1' : '0';
-    }
   };
 
   return (
-    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+    <div className="formBg">
       <div className="login-page">
         <div className="form">
-          <form className={isLoginForm ? "login-form" : "register-form"} method="POST">
-            <h2>{isLoginForm ? "Inicio de Sesión" : "Registro"}</h2>
-            {isLoginForm ? null : <input type="text" placeholder="Nombre Completo*" required />}
-            <input type="text" placeholder="Usuario*" required />
-            {isLoginForm ? null : <input type="email" placeholder="Email *" required />}
-            <input type="password" placeholder="Contraseña *" required />
-            <a className="btn" href="#">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              {isLoginForm ? "Inicia Sesión" : "Crear cuenta"}
-            </a>
-            <p className="message">
-              {isLoginForm ? "No te has registrado? " : "Ya estás registrado? "}
-              <a href="#" onClick={toggleForm}>
-                {isLoginForm ? "Crea una cuenta" : "Inicia Sesión"}
-              </a>
-            </p>
-          </form>
+          <fieldset>
+            <legend>Formulario</legend>
+            <form
+              className={isLoginForm ? "login-form" : "register-form"}
+              method="POST"
+            >
+              <h2>{isLoginForm ? "Inicio de Sesión" : "Registro"}</h2>
+              {!isLoginForm && (
+                <div>
+                  <label className="form-label">Nombre</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Nombre Completo*"
+                    required
+                  />
+                  
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email @"
+                    required
+                  />
+                </div>
+              )}
+              <label className="form-label">Usuario</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre de usuario"
+                required
+              />
+              <label className="form-label">Contraseña</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña *"
+                required
+              />
+              <button className="boton" href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                {isLoginForm ? "Iniciar Sesión" : "Crear cuenta"}
+              </button>
+              <p className="message">
+                {isLoginForm
+                  ? "Aun no te has registrado? "
+                  : "Ya tienes una cuenta? entonces ve a "}
+                <a href="#" onClick={toggleForm}>
+                  {isLoginForm ? "Crea una cuenta" : "Iniciar Sesión"}
+                </a>
+
+              
+              </p>
+            </form>
+          </fieldset>
         </div>
       </div>
     </div>
