2aba9d543fdaf75757cf03c6b86526754edc4306
Marvin Artiga AG230133 <marvin.artigagonzalez@gmail.com>
2023-10-25 23:02:28 -0600

    agregar cambios

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 import "./app.js";
+import Registro from './app.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <>
-    hola
+    <Registro></Registro>
     </>
 )
