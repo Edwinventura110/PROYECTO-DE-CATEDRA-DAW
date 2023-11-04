da488cfdd96d27bd07ee282712dfb60e3d977e9e
Marvin Artiga AG230133 <marvin.artigagonzalez@gmail.com>
2023-11-03 20:10:23 -0600

    borrar

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Registro from "./app";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <Registro></Registro>
-); 
